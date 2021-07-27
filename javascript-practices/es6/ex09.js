8f3b6b5d1046e471e3bbf265c83cc7a2051fd594
yeominji <lucky9834@naver.com>
2021-07-27 12:44:44 +0900

    ex09

---
+++ javascript-practices/es6/ex09.js
@@ -0,0 +1,19 @@
+const.foods = [
+    {no:1, name: 'Bread', quantity: 10},
+    {no:2, name: 'Egg', quantity: 20},
+    {no:3, name:'Milk', quantity: 50}
+];
+
+
+const arr1 =[]
+foods.forEach(function(food,index){
+    arr1[index]={no:food.no,n:food.name,Q:food.quantity};
+});
+console.log(arr1);
+
+
+foods .map(function(food){
+    return {no:food.no,n:food.name,Q:food.quantity};
+
+});
+console.log(arr2);
