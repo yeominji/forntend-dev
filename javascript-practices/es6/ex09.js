225dd760833fdeaa31d2986e91bb594b1c6d0b53
yeominji <lucky9834@naver.com>
2021-08-18 09:49:41 +0900

    .DS_Store remove

--- javascript-practices/es6/ex09.js
+++ javascript-practices/es6/ex09.js
@@ -1,4 +1,4 @@
-const.foods = [
+const foods = [
     {no:1, name: 'Bread', quantity: 10},
     {no:2, name: 'Egg', quantity: 20},
     {no:3, name:'Milk', quantity: 50}
---
+++ javascript-practices/es6/ex10.js
@@ -0,0 +1,9 @@
+const foods = [
+    {no:1, name: 'Bread', quantity: 10},
+    {no:2, name: 'Egg', quantity: 20},
+    {no:3, name:'Milk', quantity: 50},
+    {no:4, name:'Carrot', quantity: 50}
+];
+const food =foods.filter(function(food => food.quantity <=20);
+
+
